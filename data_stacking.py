c324abeb79d168ae46c976febf9632cc6a9b020f
berylcczhang <berylcczhang@gmail.com>
2021-06-14 17:10:24 -0400

    stacked data

---
+++ data_stacking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 14:37:54 2021
+
+@author: beryl
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+path = './Toxin/210608/ch2_TcDA_SC_pH7_lp10_210608'
+data_file = [f for f in os.listdir(path) if f.endswith('good.dat')]
+data_stack = []
+
+for files in data_file:
+    directory = path + '/' + files
+    data = np.loadtxt(directory)
+    data_stack.append(data)
+
+data_stack = np.concatenate(data_stack,axis=1)
+data_stack = data_stack[10:150,:]
+
+np.savetxt(path + '/stacked_data.dat', data_stack, fmt='%8.1f' )
+
